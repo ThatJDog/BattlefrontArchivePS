09f9b6cf7b920d8bcbe459d030cbc5b290e8161e
ThatJDog <joelleonardbest@gmail.com>
2025-01-29 01:18:22 +0000

    Updated Parser and Lexer with new CFG

---
+++ Files/PDML/PDMLListener.js
@@ -0,0 +1,126 @@
+// Generated from PDML.g4 by ANTLR 4.13.2
+// jshint ignore: start
+// import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by PDMLParser.
+export default class PDMLListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by PDMLParser#grammarSpec.
+	enterGrammarSpec(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#grammarSpec.
+	exitGrammarSpec(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#body.
+	enterBody(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#body.
+	exitBody(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#tag.
+	enterTag(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#tag.
+	exitTag(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#open_tag.
+	enterOpen_tag(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#open_tag.
+	exitOpen_tag(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#close_tag.
+	enterClose_tag(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#close_tag.
+	exitClose_tag(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#self_closing_tag.
+	enterSelf_closing_tag(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#self_closing_tag.
+	exitSelf_closing_tag(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#valueAssignment.
+	enterValueAssignment(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#valueAssignment.
+	exitValueAssignment(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#boolLit.
+	enterBoolLit(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#boolLit.
+	exitBoolLit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#numLit.
+	enterNumLit(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#numLit.
+	exitNumLit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#stringLit.
+	enterStringLit(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#stringLit.
+	exitStringLit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#hexColour.
+	enterHexColour(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#hexColour.
+	exitHexColour(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#struct.
+	enterStruct(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#struct.
+	exitStruct(ctx) {
+	}
+
+
+	// Enter a parse tree produced by PDMLParser#array.
+	enterArray(ctx) {
+	}
+
+	// Exit a parse tree produced by PDMLParser#array.
+	exitArray(ctx) {
+	}
+
+
+
+}
